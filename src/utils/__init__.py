ba21c2581f4118dc1977d355f8a555e3b6b567a4
znesonerf-hue <znesonerf@gmail.com>
2026-07-31 14:51:12 +0700

    Fix src/utils/__init__.py import paths

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,31 +1,30 @@
 """Utility modules"""
 
 from .logger import setup_logger, logger
-from .config import config, Config
-from .helpers import (
-    calculate_rsi,
-    calculate_macd,
-    calculate_bollinger_bands,
-    calculate_atr,
-    format_price,
-    format_percentage,
-    calculate_time_until_next_candle,
-    calculate_position_size,
-    round_to_precision
-)
+
+try:
+    from .config import config, Config
+except ImportError:
+    # Config is not in utils, may be located elsewhere
+    pass
+
+try:
+    from .helpers import (
+        calculate_rsi,
+        calculate_macd,
+        calculate_bollinger_bands,
+        calculate_atr,
+        format_price,
+        format_percentage,
+        calculate_time_until_next_candle,
+        calculate_position_size,
+        round_to_precision
+    )
+except ImportError:
+    # Helpers not yet in utils
+    pass
 
 __all__ = [
     'setup_logger',
     'logger',
-    'config',
-    'Config',
-    'calculate_rsi',
-    'calculate_macd',
-    'calculate_bollinger_bands',
-    'calculate_atr',
-    'format_price',
-    'format_percentage',
-    'calculate_time_until_next_candle',
-    'calculate_position_size',
-    'round_to_precision'
 ]
